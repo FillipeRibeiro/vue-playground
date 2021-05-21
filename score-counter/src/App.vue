45e5544825cf40250d367874644b98f305d96ae4
FillipeRibeiro <fillipe@imaginationmedia.com>
2021-05-20 21:19:57 -0300

    Adding score-counter

---
+++ score-counter/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app" class="container">
+    <h1 class="text-center">Score Counter</h1>
+    <counter name="Player 1"></counter>
+    <counter name="Player 2"></counter>
+  </div>
+</template>
+
+<script>
+import Counter from "./components/Counter";
+
+export default {
+  name: "App",
+  components: {
+    Counter,
+  },
+};
+</script>
---
+++ score-counter/src/components/Counter.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="card">
+    <div class="card-body text-center">
+      <h3>{{ name }}</h3>
+      <button @click="increaseScore" type="button" class="btn btn-success">
+        Up
+      </button>
+      <span class="player-score">{{ player_score }}</span>
+      <button @click="decreaseScore" type="button" class="btn btn-danger">
+        Down
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+  },
+  data() {
+    return {
+      player_score: 0,
+    };
+  },
+  methods: {
+    increaseScore() {
+      this.player_score += 1;
+    },
+    decreaseScore() {
+      if (this.player_score > 0) {
+        this.player_score -= 1;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.player-score {
+  font-size: 4rem;
+}
+</style>
